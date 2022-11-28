de54a52aab869ee287f5298fa099621624af2694
ni-ssay <amraouiyassin726@gmail.com>
2022-11-28 17:00:39 +0100

    add first step

--- api.php
+++ api.php
@@ -6,6 +6,31 @@ error_reporting(0);
 set_time_limit(0);
 date_default_timezone_set('Asia/Jakarta');
 
+//frist Step 
+function getSID(){
+  $url = 'https://graasp.eu/donate';
+  $post_items[] = "amount=1";
+  $payload = implode ('&', $post_items);
+
+  //create cURL connection
+  $ch =  curl_init();
+  //set options
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5000);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+  curl_setopt($ch, CURLOPT_HEADER, TRUE);
+
+  //perform our request
+  $response = curl_exec($ch);
+  //close the connection
+  curl_close($ch);
+  return $response;
+}
+
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     extract($_POST);
 } elseif ($_SERVER['REQUEST_METHOD'] == "GET") {
@@ -59,6 +84,7 @@ function mod($dividendo,$divisor)
     return round($dividendo - (floor($dividendo/$divisor)*$divisor));
 }
 
+$sessionId = getSID();
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://randomuser.me/api/?nat=us');
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
